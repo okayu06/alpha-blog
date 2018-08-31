bb66f4e453b85fbd394a7f994978a835f92d9836
Yu Hiraoka <magicsunset0805@gmail.com>
2018-08-31 09:54:10 +0000

    Add edit, update,show andlisting

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,22 @@
 class ArticlesController < ApplicationController
   
+  def index
+   @articles = Article.all  
+  end
+  
   def new 
     @article =Article.new
   end  
+  
+  def edit 
+    @article = Article.find(params[:id])
+  end
   
   def create
     #render plain: params[:article].inspect
     @article = Article.new(article_params)
     if @article.save
+      #flashというのは内部的に何をしているのかというと、HTTPレスポンスを返す際にそのレスポンスヘッダーにそのメッセージを仕込ませるという処理を行っています。その都度レスポンスヘッダーに情報を渡すだけなので、このflashによるメッセージが表示されるのは一回限りです。ブラウザの更新ボタンを押したりして再度リクエストを送るとメッセージは表示されません。
       flash[:notice] = "Article was successfully saved"
       redirect_to article_path(@article)
     else
@@ -17,6 +26,17 @@ class ArticlesController < ApplicationController
     #@article.save
     #redirect_to article_path(@articles)
   end  
+  
+  def update 
+    @article = Article.find(params["id"])
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end  
+  end
+    
   
   def show 
     @article = Article.find(params[:id])
