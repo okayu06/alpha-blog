a919a10c3d544c330d6a6b5a88497e35ab3dda8a
Yu Hiraoka <magicsunset0805@gmail.com>
2018-08-31 10:54:55 +0000

    Add partials and destroy action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,7 +18,7 @@ class ArticlesController < ApplicationController
     if @article.save
       #flashというのは内部的に何をしているのかというと、HTTPレスポンスを返す際にそのレスポンスヘッダーにそのメッセージを仕込ませるという処理を行っています。その都度レスポンスヘッダーに情報を渡すだけなので、このflashによるメッセージが表示されるのは一回限りです。ブラウザの更新ボタンを押したりして再度リクエストを送るとメッセージは表示されません。
       flash[:notice] = "Article was successfully saved"
-      redirect_to article_path(@article)
+      redirect_to articles_path
     else
       #newtemplateをrenderinsg
       render 'new'
@@ -40,6 +40,13 @@ class ArticlesController < ApplicationController
   
   def show 
     @article = Article.find(params[:id])
+  end  
+  
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "destruction was successful"
+    redirect_to articles_path
   end  
   
   private
